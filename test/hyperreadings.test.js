274aa6b9e112700b4e1b9e6849945625aeadb7eb
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-04-02 22:49:00 +1000

    crawling towards a definition based model of hyper readings - very much work in progress

--- test/hyperreadings.test.js
+++ test/hyperreadings.test.js
@@ -5,29 +5,50 @@
 
 var hyperreadings = require('../lib/hyperreadings')
 // var utils = require('../lib/utils')
-var fs = require('fs')
+// var fs = require('fs')
+
+// describe('hyperreadings', () => {
+//   let readings
+//   before(done => {
+//     const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
+//     hyperreadings.loadFromMarkdown(file.toString(), null, (err, hr) => {
+//       if (err) return done(err)
+//       readings = hr
+//       done()
+//     })
+//   })
+
+//   it('returns the root', () => {
+//     function processContents (contents) {
+//       return readings.get(contents.id)
+//         // .then(value => console.log(contents.id, JSON.stringify(value, null, 2)))
+//         .then(() => readings.contents(contents.id, processContents))
+//     }
+//     console.time('iterate')
+//     return readings.contents(null, processContents)
+//       .then(() => {
+//         console.timeEnd('iterate')
+//       })
+//   }).timeout(10000)
+// })
 
 describe.only('hyperreadings', () => {
-  let readings
+  let hr
   before(done => {
-    const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
-    hyperreadings.loadFromMarkdown(file.toString(), null, (err, hr) => {
-      if (err) return done(err)
-      readings = hr
-      done()
-    })
+    // const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
+    hr = hyperreadings.HyperReadings()
+    hr.on('ready', done)
   })
 
-  it('returns the root', () => {
-    function processContents (contents) {
-      return readings.get(contents.id)
-        .then(value => console.log(contents.id, JSON.stringify(value, null, 2)))
-        .then(() => readings.contents(contents.id, processContents))
-    }
-    console.time('iterate')
-    return readings.contents(null, processContents)
-      .then(() => {
-        console.timeEnd('iterate')
-      })
-  }).timeout(10000)
+  it('just works', async () => {
+    var r = await hr.root()
+    await hr.createNode('hr:root')
+    r = await hr.root()
+    console.log(r)
+    var rootNode = await hr.node(r)
+    console.log(rootNode)
+    var section = await hr.createNode('doco:section')
+    await rootNode.insertNode(section)
+
+  })
 })
--- test/markdown-to-triples.test.js
+++ test/markdown-to-triples.test.js
@@ -1,53 +1,53 @@
-/* eslint-env mocha */
+// /* eslint-env mocha */
 
-// var expect = require('chai').expect
-// var jsonld = require('jsonld')
-var hypergraph = require('hyper-graph-db')
-var ram = require('random-access-memory')
+// // var expect = require('chai').expect
+// // var jsonld = require('jsonld')
+// var hypergraph = require('hyper-graph-db')
+// var ram = require('random-access-memory')
 
-var markdown = require('../lib/markdown-to-triples')
-// var utils = require('../lib/utils')
-var fs = require('fs')
+// var markdown = require('../lib/markdown-to-triples')
+// // var utils = require('../lib/utils')
+// var fs = require('fs')
 
-describe('toStream', () => {
-  it('returns a stream of triples representing the document', (done) => {
-    const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
-    // '# Hello *world*!!!'
-    // const test = '# yes\n\nno\nmaybe'
-    const input = markdown.toStream(file.toString())
-    const graph = hypergraph(() => ram())
-    const put = graph.putStream()
-    input.on('error', done)
-    input.pipe(put)
+// describe('toStream', () => {
+//   it('returns a stream of triples representing the document', (done) => {
+//     const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
+//     // '# Hello *world*!!!'
+//     // const test = '# yes\n\nno\nmaybe'
+//     const input = markdown.toStream(file.toString())
+//     const graph = hypergraph(() => ram())
+//     const put = graph.putStream()
+//     input.on('error', done)
+//     input.pipe(put)
 
-    put.on('finish', () => {
-      // done()
-      setTimeout(() => {
-        console.time('stream')
-        var r = graph.db.createReadStream('')
-        r.on('data', (d) => {
-          // console.log('read')
-          console.log('-', d[0].value.toString())
-        })
-        r.on('end', () => {
-          console.timeEnd('stream')
-          done()
-        })
-      }, 1)
-      // graph.get({object: 'yes'}, (err, node) => {
-      // // graph.get({predicate: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value'}, (err, node) => {
-      //   console.log(err)
-      //   console.log(node)
-      //   done()
-      // })
-    })
-    // jsonld.toRDF(doc, {format: 'application/n-quads'}, (err, no) => {
-    //   console.log('node')
-    //   N3.Parser({ format: 'N-Quads' }).parse(no, (err, n) => {
-    //     console.log(n)
-    //   })
-    // })
-    // hypergraph.put()
-    // markdown.parse(file.toString())
-  })
-})
+//     put.on('finish', () => {
+//       // done()
+//       setTimeout(() => {
+//         console.time('stream')
+//         var r = graph.db.createReadStream('')
+//         r.on('data', (d) => {
+//           // console.log('read')
+//           console.log('-', d[0].value.toString())
+//         })
+//         r.on('end', () => {
+//           console.timeEnd('stream')
+//           done()
+//         })
+//       }, 1)
+//       // graph.get({object: 'yes'}, (err, node) => {
+//       // // graph.get({predicate: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value'}, (err, node) => {
+//       //   console.log(err)
+//       //   console.log(node)
+//       //   done()
+//       // })
+//     })
+//     // jsonld.toRDF(doc, {format: 'application/n-quads'}, (err, no) => {
+//     //   console.log('node')
+//     //   N3.Parser({ format: 'N-Quads' }).parse(no, (err, n) => {
+//     //     console.log(n)
+//     //   })
+//     // })
+//     // hypergraph.put()
+//     // markdown.parse(file.toString())
+//   })
+// })
