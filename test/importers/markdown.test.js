103f4e9e09a873220dea7cb9db1ea6022039abe4
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-04-24 21:57:20 +1000

    add new importers

---
+++ test/importers/markdown.test.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+
+// var expect = require('chai').expect
+// var fs = require('fs')
+var ram = require('random-access-memory')
+var hyperreadings = require('../../lib/hyperreadings').HyperReadings
+var markdownImporter = require('../../lib/importers/markdown')
+
+describe('markdownImporter', () => {
+  it('imports markdown file into a hyper readinglist', async () => {
+    // const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
+    // '# Hello *world*!!!'
+    const test = '# yes\n\nno\nmaybe'
+    const hr = hyperreadings(() => ram())
+    await markdownImporter(hr, test) //file.toString())
+    // const base = await hr.root()
+    // await base.iterate(print)
+    // async function print (node) {
+    //   console.log(node.name, node.type)
+    //   console.log(await node.get('rdf:value'))
+    //   if (node.iterate) await node.iterate(print)
+    // }
+  }).timeout(6000)
+})
