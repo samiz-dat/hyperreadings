872b3648d3fa5f132423228ff5486262df9205f8
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-04-25 21:01:19 +1000

    begining to think about how to deal with rendering hyperreadings

--- test/importers/markdown.test.js
+++ test/importers/markdown.test.js
@@ -1,17 +1,24 @@
 /* eslint-env mocha */
 
 // var expect = require('chai').expect
-// var fs = require('fs')
+var fs = require('fs')
 var hyperreadings = require('../../lib/hyperreadings')
 var markdownImporter = require('../../lib/importers/markdown')
 
 describe('markdownImporter', () => {
-  it('imports markdown file into a hyper readinglist', async () => {
-    // const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
-    // '# Hello *world*!!!'
-    const test = '# yes\n\nno\nmaybe'
+  it.only('imports markdown file into a hyper readinglist', async () => {
+    const file = fs.readFileSync('./reading-lists/hyper-graph-db-research.md')
+    // const test = '# Hello *world*!!!'
+    // const test = '# yes\n\nno\nmaybe'
     const hr = hyperreadings()
-    await markdownImporter(hr, test) // file.toString())
+    // await markdownImporter(hr, test); //file.toString())
+    await markdownImporter(hr, file.toString())
+    const stream = await hr.renderStream()
+    return new Promise((resolve, reject) => {
+      stream.on('data', data => data && console.log(data.name, '->', data.type))
+      stream.on('end', resolve)
+      stream.on('error', reject)
+    })
     // const base = await hr.root()
     // await base.iterate(print)
     // async function print (node) {
