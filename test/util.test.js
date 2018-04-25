f1fcf28e41797d09bfed1313917f345eea4e0fa0
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-04-25 11:21:13 +1000

    remove unused utils

--- test/util.test.js
+++ test/util.test.js
@@ -1,26 +1,17 @@
 /* eslint-env mocha */
 
-var expect = require('chai').expect
-var utils = require('../lib/utils')
-var Readable = require('stream').Readable
+// var expect = require('chai').expect
+// var utils = require('../lib/utils')
 
-describe('arrayToStream', () => {
-  it('throws an error if not passed an array as an argument', () => {
-    const dudArguments = [undefined, null, 1, { length: 2 }, 'hello']
-    dudArguments.forEach((arg) => expect(utils.arrayToStream.bind(null, arg)).to.throw(/arrayToStream expects array as first argument/))
-  })
-  it('takes an array and returns a stream', (done) => {
-    const input = [1, 2, 3]
-    const expected = [1, 2, 3]
-    var stream = utils.arrayToStream(input)
-    expect(stream).to.be.instanceOf(Readable)
-    stream.on('data', (value) => {
-      expect(value).to.eql(expected.shift())
-    })
-    stream.on('end', () => {
-      expect(expected.length).to.eql(0)
-      done()
-    })
-    stream.on('error', done)
-  })
+describe('isRdfLiteral', () => {
+  it('needs to be tested')
+})
+describe('fromRdfValue', () => {
+  it('needs to be tested')
+})
+describe('isNode', () => {
+  it('needs to be tested')
+})
+describe('toRdfValue', () => {
+  it('needs to be tested')
 })
